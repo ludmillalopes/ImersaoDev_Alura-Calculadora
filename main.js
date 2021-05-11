b684f5a160e62b2eb0f2a0c2b08e1243267621fc
Ludmilla Lopes <ludmilla.lopes@hotmail.com>
2021-05-10 23:26:53 -0300

    Correção do código

--- main.js
+++ main.js
@@ -1,15 +1,6 @@
 var primeiroValor = parseInt(prompt("Digite o primeiro valor:"))
 var segundoValor = parseInt(prompt("Digite o segundo valor:"))
 var resultado = primeiroValor * segundoValor
-/*
-var primeiroValor = prompt("Digite o primeiro valor:")
-var primeiroValorInt = parseInt(primeiroValor)
-
-var segundoValor = prompt("Digite o segundo valor:")
-var segundoValorInt = parseInt(segundoValor)
-
-var resultado = primeiroValorInt * segundoValorInt
-*/
 
 var operacao = prompt("Digite 1 para fazer uma divisão, 2 para multiplicação, 3 para soma e 4 para subtração:")
 if (operacao == 1) {
@@ -28,6 +19,3 @@ if (operacao == 1) {
 else {
   document.write("<h2>Opção inválida</h2>")
 }
-
-
-//document.write("<h2>" + primeiroValor + " x " + segundoValor + " = " + resultado + "<h2/>")
